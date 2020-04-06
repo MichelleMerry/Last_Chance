15612126f14def1e6af45b1a3cba18fa8631cdff
DropBear25 <60543506+DropBear25@users.noreply.github.com>
2020-04-06 12:34:36 +0100

    please work

---
+++ Last Chance/Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+
+    public void quit()
+    {
+        Debug.Log("Quit the game");
+        Application.Quit();
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
